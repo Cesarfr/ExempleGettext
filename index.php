f4577bd99cb2ca3aa88815d5bb36460a9063d78f
Cesar HERNANDEZ ANTONIO <hernandez.antonio.cesar19@gmail.com>
2015-03-31 01:23:25 +0200

    Update of layout and change dynamic the language

--- index.php
+++ index.php
@@ -1,9 +1,29 @@
 <?php
-	$language = "fr_FR.utf8";
+	if ($_GET['l']=="") {
+		$language = "es_MX";
+	} else {
+		switch ($_GET['l']) {
+			case 'es': $language = "es_MX"; break;
+			case 'fr': $language = "fr_FR.utf8"; break;	
+			case 'en': $language = "en_US.utf8"; break;			
+			default: $language = "es_MX"; break;
+		}
+	}
 	putenv("LC_ALL=$language");
 	setlocale(LC_ALL, $language);
 	bindtextdomain("messages", "./locale");
 	textdomain("messages");
-
-	print "<p>" . utf8_decode(_("Esta página está en español")) . "</p>\n";
 ?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<meta charset="UTF-8">
+	<title>Example Gettext</title>
+</head>
+<body>
+	<?= "<h1>"._("Esta página está en español")."</h1>" ?>
+	<a href="<?= $_SERVER['PHP_SELF'].'?l=en' ?>">English</a> -
+  	<a href="<?= $_SERVER['PHP_SELF'].'?l=es' ?>">Español</a> -
+  	<a href="<?= $_SERVER['PHP_SELF'].'?l=fr' ?>">Français</a>
+</body>
+</html>
